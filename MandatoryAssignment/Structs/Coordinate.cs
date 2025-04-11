86db7974c560e29a46eca25524b3f238e1246b38
Marius Egede Knudsen <mak004@edu.zealand.dk>
2025-04-11 09:53:21 +0200

    added PosititveInt to represent numbers in the 0 to Int.Max range

--- MandatoryAssignment/Structs/Coordinate.cs
+++ MandatoryAssignment/Structs/Coordinate.cs
@@ -1,15 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MandatoryAssignment.Structs
 {
-    public struct Coordinate
+    public readonly struct Coordinate
     {
-        public uint x;
-        public uint y;
+        public readonly uint x;
+        public readonly uint y;
         public Coordinate(uint x, uint y)
         {
             this.x = x; 
@@ -19,5 +20,35 @@ namespace MandatoryAssignment.Structs
         {
             return new Coordinate(a.x + b.x, a.y + a.x);
         }
+        public override bool Equals(object? obj)
+        {
+            if(obj is not null && obj is Coordinate other)
+            {
+                if(other.x == this.x && other.y == this.y)
+                {
+                    return true;
+                }
+            }
+            return false;
+            
+        }
+        public override int GetHashCode()
+        {
+            uint hash = 23;
+            hash = hash * 31 + x;
+            hash = hash * 31 + y;
+            return hash;
+        }
+
+        public static bool operator ==(Coordinate left, Coordinate right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Coordinate left, Coordinate right)
+        {
+            return !(left == right);
+        }
+        
     }
 }
---
+++ MandatoryAssignment/Structs/PositiveInt.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MandatoryAssignment.Structs
+{
+    /// <summary>
+    /// Represents a integer in the range of 0 to int.Max
+    /// </summary>
+    public readonly struct PositiveInt
+    {
+        public readonly int _value;
+
+        public int Value { get { return _value; } }
+
+        public PositiveInt(int value)
+        {
+            if(value < 0)
+            {
+                throw new ArgumentOutOfRangeException("value");
+            }
+            _value = value;
+        }
+
+        public static implicit operator PositiveInt(int value)
+        {
+            return new PositiveInt(value);
+        }
+        public static PositiveInt operator +(PositiveInt value1,int value2)
+        {
+            return new PositiveInt(value1.Value + value2);
+        }
+        public static PositiveInt operator +(PositiveInt value1, PositiveInt value2)
+        {
+            return new PositiveInt(value1.Value + value2.Value);
+        }
+        public static PositiveInt operator -(PositiveInt value1, PositiveInt value2)
+        {
+            return new PositiveInt(value1.Value - value2.Value);
+        }
+        public static PositiveInt operator -(PositiveInt value1, int value2)
+        {
+            return new PositiveInt(value1.Value - value2);
+        }
+        public static bool operator ==(PositiveInt left, PositiveInt right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(PositiveInt left, PositiveInt right)
+        {
+            return !(left == right);
+        }
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+        public override bool Equals([NotNullWhen(true)] object? obj)
+        {
+            return Value.Equals(obj);
+        }
+        public override int GetHashCode()
+        {
+            return Value;
+        }
+
+    }
+}
